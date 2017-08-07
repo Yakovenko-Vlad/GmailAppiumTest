32a86b6c0388573996e552ac84fcb0a1f1ad1050
IT-40016\user <yakovenkovl12@gmail.com>
2017-08-07 12:55:52 +0300

    Add tittle to test

--- src/test/java/tests/GmailTest.java
+++ src/test/java/tests/GmailTest.java
@@ -4,9 +4,11 @@ import org.testng.annotations.Test;
 import pages.EmailListPage;
 import pages.NewEmailPage;
 import pages.NewInputEmailPage;
+import ru.yandex.qatools.allure.annotations.Title;
 import utils.base.BaseTest;
 
 public class GmailTest extends BaseTest {
+    @Title("Send new email and check it")
     @Test(priority = 0)
     public void newEmailTest() {
         NewEmailPage newEmailPage = new EmailListPage(driver).openNewEmailPage();
